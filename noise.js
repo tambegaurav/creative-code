8ae130969185889ba5b11dcc3549aa337069db74
Gaurav <gauravkishortambe@gmail.com>
2021-05-19 17:32:37 +0530

    add noisy art

--- noise.js
+++ noise.js
@@ -10,33 +10,35 @@ const settings = {
 };
 //UV space --> between 0 and 1
 const sketch = () => {
-  const colorCount = random.rangeFloor(1, 6);
+  const colorCount = random.rangeFloor(2, 6);
   const palette = random.pick(palettes).slice(0, colorCount);
 
   console.log(palette);
   const createGrid = () => {
     const points = [];
-    const count = 20;
+    const count = 70;
 
     for (var x = 0; x < count; x++) {
       for (var y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1); // count-1 --> u,v starts from 0 but not go till 1, but 0.8
         const v = count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v)) * 0.25;
         points.push({
-          radius: Math.abs(0.015 + random.gaussian() * 0.02),
+          radius,
           positions: [u, v],
           color: random.pick(palette),
+          rotation: random.noise2D(u, v),
         });
       }
     }
     return points;
   };
 
-  random.setSeed(72);
+  //   random.setSeed(72);
 
-  const points = createGrid().filter(() => random.value() > 0.55);
+  const points = createGrid().filter(() => random.value() > 0.7);
   console.log(points);
-  const margin = 250;
+  const margin = 20;
 
   return ({ context, width, height }) => {
     context.fillStyle = "white";
@@ -45,17 +47,30 @@ const sketch = () => {
     points.forEach((data) => {
       //   const x = u * width;
       //   const y = v * height;
-      const { radius, positions, color } = data;
+      const { radius, positions, color, rotation } = data;
 
       const [u, v] = positions;
 
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, radius * width, 0, 2 * Math.PI, false);
+      //circles
+      //   context.beginPath();
+      //   context.arc(x, y, radius * width, 0, 2 * Math.PI, false);
+      //   context.fillStyle = color;
+      //   context.fill();
+
+      //texts
+
+      const texts = ["=", "$", "#", "@", "&"];
+
+      context.save();
       context.fillStyle = color;
-      context.fill();
+      context.font = `${radius * width}px "Helvetica"`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText("I", 0, 0);
+      context.restore();
     });
   };
 };
